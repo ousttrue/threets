a3c00778d17a9bf89a537a1ba2169908139dafe8
ousttrue <ousttrue@gmail.com>
2024-01-22 16:55:30 +0900

    revert

--- src/leva.stories.tsx
+++ src/leva.stories.tsx
@@ -6,11 +6,9 @@ import { xyz, xyzScaleColorWire } from "./libs/ui";
 export function PositionXYZStory() {
   const { positionX, positionY, positionZ } = xyz();
   return (
-    <div>
-      <Canvas>
-        <Box position={[positionX, positionY, positionZ]} />
-      </Canvas>
-    </div>
+    <Canvas>
+      <Box position={[positionX, positionY, positionZ]} />
+    </Canvas>
   );
 }
 
@@ -18,23 +16,22 @@ export function PositionXYZScaleCameraStory() {
   const { scale, positionX, positionY, positionZ, color, wireframe } =
     xyzScaleColorWire();
   return (
-    <div>
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} />
-        {/* <mesh>
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[0, 0, 5]} />
+      {/* <mesh>
           <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial color="red" />
         </mesh> */}
-        <Box
-          args={[2, 2, 2]}
-          scale={scale}
-          position={[positionX, positionY, positionZ]}
-        >
-          <meshStandardMaterial color={color} wireframe={wireframe} />
-        </Box>
-        <OrbitControls />
-      </Canvas>
-    </div>
+      <Box
+        args={[2, 2, 2]}
+        scale={scale}
+        position={[positionX, positionY, positionZ]}
+      >
+        <meshStandardMaterial color={color} wireframe={wireframe} />
+      </Box>
+      <OrbitControls />
+    </Canvas>
   );
 }
+// PositionXYZScaleCameraStory.meta = { iframed: true };
