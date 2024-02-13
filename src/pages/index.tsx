1bea33758c683f009e7382bb649a7c9331586611
ousttrue <ousttrue@gmail.com>
2024-02-13 21:48:48 +0900

    drop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import Layout from "@theme/Layout";
 import { Canvas } from "@react-three/fiber";
 import { Box, Grid, OrbitControls, TransformControls } from "@react-three/drei";
+import { useDropzone } from "react-dropzone";
+import { GLTFLoader, GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 let resizeObserver: ResizeObserver | null = null;
 
 export default function GlCanvs() {
+  const [gltf, setGltf] = React.useState<GLTF>();
   const ref = React.useRef<HTMLDivElement>(null);
   const [size, setSize] = React.useState([300, 150]);
   React.useEffect(() => {
@@ -31,9 +34,32 @@ export default function GlCanvs() {
     resizeObserver.observe(ref.current);
   }, []);
 
+  const onDrop = React.useCallback(async (acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    if (!file) {
+      return;
+    }
+    const buffer = await file.arrayBuffer();
+
+    const loader = new GLTFLoader();
+    // loader.register((parser) => {
+    //   return new VRMLoaderPlugin(parser);
+    // });
+
+    const gltf = await loader.parseAsync(buffer, file.name);
+    setGltf(gltf);
+    console.log("loaded", gltf);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
     <Layout>
       <div
+        {...getRootProps({
+          onClick: (event) => {
+            event.stopPropagation();
+          },
+        })}
         ref={ref}
         style={{
           flexGrow: 1,
@@ -50,7 +76,7 @@ export default function GlCanvs() {
             fov: 60,
             near: 0.1,
             far: 1000,
-            position: [0, 1.6, 4],
+            position: [0, 1.6, 3],
           }}
         >
           <color attach="background" args={[0, 0, 0]} />
@@ -59,9 +85,7 @@ export default function GlCanvs() {
           <directionalLight position={[10, 10, 5]} />
           <OrbitControls makeDefault />
           <Grid cellColor="white" args={[10, 10]} />
-          <Box position={[0, 0.5, 0]}>
-            <meshStandardMaterial />
-          </Box>
+          {gltf ? <primitive object={gltf.scene} /> : null}
         </Canvas>
       </div>
     </Layout>
